401db762539a859ec8af5e3c0717c0a89c0b4817
Luke Bennett <hello@lukebennett.com.au>
2024-04-05 21:40:11 +1100

    Rename cookie

--- app/theme.server.ts
+++ app/theme.server.ts
@@ -2,22 +2,22 @@ import * as cookie from 'cookie';
 
 const ONE_YEAR = 31_536_000;
 
-const cookieName = 'en_theme';
+const THEME_COOKIE_NAME = 'theme';
 export type Theme = 'light' | 'dark';
 
 export function getTheme(request: Request): Theme | null {
 	const cookieHeader = request.headers.get('cookie');
 	const parsed =
-		cookieHeader ? cookie.parse(cookieHeader)[cookieName] : 'light';
+		cookieHeader ? cookie.parse(cookieHeader)[THEME_COOKIE_NAME] : 'light';
 	if (parsed === 'light' || parsed === 'dark') return parsed;
 	return null;
 }
 
 export function setTheme(theme: Theme | 'system') {
 	if (theme === 'system') {
-		return cookie.serialize(cookieName, '', { path: '/', maxAge: -1 });
+		return cookie.serialize(THEME_COOKIE_NAME, '', { path: '/', maxAge: -1 });
 	} else {
-		return cookie.serialize(cookieName, theme, {
+		return cookie.serialize(THEME_COOKIE_NAME, theme, {
 			path: '/',
 			maxAge: ONE_YEAR,
 		});
